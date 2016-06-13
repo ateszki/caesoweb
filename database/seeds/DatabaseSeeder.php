e796fad3ea4116a6f006c407805c69c0054e3e3f
ateszki <ateszki@gmail.com>
2016-06-13 18:06:16 -0300

    bolsa de trabajo users test3

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -55,7 +55,7 @@ class UserTableSeeder extends Seeder
                     'email' => $user[1],
                     'password' => bcrypt($pass),
                 ]);
-                Mail::send('auth.email-user', array('email' => $user[1],'clave'=>$pass), function($message)
+                Mail::send('auth.email-user', array('email' => $user[1],'clave'=>$pass), function($message) use ($user)
                 {
                     $message->to($user[1], $user[0])->subject('Accesoa  consulta de la bolsa de trabajod e Caeso');
                 });
