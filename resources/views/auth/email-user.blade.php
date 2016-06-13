28a02c9eb0dedbfc446d8bcc3c43aacdb9e87da9
ateszki <ateszki@gmail.com>
2016-06-13 18:04:58 -0300

    bolsa de trabajo users test2

--- resources/views/auth/email-user.blade.php
+++ resources/views/auth/email-user.blade.php
@@ -2,6 +2,6 @@
 <p>Para acceder a la consulta de la bolsa de trabajo de Caeso debe ingresar a:</p>
 <p><a href="http://www.caeso.com.ar/auth/login">http://www.caeso.com.ar/auth/login</a></p>
 <p> con las siguientes credenciales:</p>
-<div>Email: {{email}}</div>
-<div>Clave: {{clave}}</div>
+<div>Email: {{$email}}</div>
+<div>Clave: {{$clave}}</div>
 <p>¡Muchas gracias!</p>
