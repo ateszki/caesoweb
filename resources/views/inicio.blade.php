39dd79170bde2b4f865432bea52137d378dcd293
ateszki <ateszki@gmail.com>
2021-07-02 09:04:38 -0300

    saco klassmile

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -236,6 +236,7 @@
                     </div>
                 </div>
                 -->
+                <!--
                 <div class="col-sm-6 col-md-3">
                     <div class="integrante wow fadeInUp" data-wow-duration="400ms" data-wow-delay="300ms">
                         <div class="team-img">
@@ -244,6 +245,7 @@
                         </div>
                     </div>
                 </div>
+                -->
                 <div class="col-sm-6 col-md-3">
                     <div class="integrante wow fadeInUp" data-wow-duration="400ms" data-wow-delay="300ms">
                         <div class="team-img">
