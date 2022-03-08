56180608a2411b7392fceb7c858893ccfddd647b
ateszki <ateszki@gmail.com>
2022-03-08 10:00:24 -0300

    saco sonrisas argentinas

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -250,6 +250,7 @@
                     </div>
                 </div>
                 -->
+                <!--
                 <div class="col-sm-6 col-md-3">
                     <div class="integrante wow fadeInUp" data-wow-duration="400ms" data-wow-delay="300ms">
                         <div class="team-img">
@@ -258,6 +259,7 @@
                         </div>
                     </div>
                 </div>
+                -->
                 <div class="col-sm-6 col-md-3">
                     <div class="integrante wow fadeInUp" data-wow-duration="400ms" data-wow-delay="300ms">
                         <div class="team-img">
